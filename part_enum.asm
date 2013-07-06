70eac1926e1e9c7a24b674ddc3e6586bf4774d6b
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2013-07-06 04:53:12 +0200

    Added hex ascii parsing routine

--- part_enum.asm
+++ part_enum.asm
@@ -10,14 +10,17 @@ load_addr	equ 07c00h
 ; The ext2/3/4 superblock will be loaded there.
 superblock	equ load_addr
 
-; The current partition table sector being processed will be stored there.
-pblock_addr	equ superblock - sector_size
-
 ; Most of the code will be copied to and run from there.
-run_addr	equ pblock_addr - sector_size
+run_addr	equ superblock - sector_size
 
 ; Global variables will be located there.
 work_addr	equ run_addr - sector_size
+
+; Binary partition type to check for.
+ptype_off	equ work_addr
+
+; Binary UUID to check for.
+uuid_off	equ work_addr + 1
 
 ; The stack (growing down) ends there (exclusive).
 stacktop_addr	equ work_addr
@@ -91,32 +94,51 @@ s1start:	cli
 		mov sp,stacktop_addr
 		cld
 		sti
+		
+		; Parse ascii hex literals into binary for later comparison.
+		mov si,ascii_ptype
+		mov di,ptype_off
+		call parse_hex
+		mov si,ascii_uuid
+		mov di,uuid_off
+		call parse_hex
 
-		; Copy the partition table.
+		; Copy the remaining code.
 		;
 		; We don't change the offsets relative to the start of the
 		; sector, allowing easier manual code review. Also there is
 		; enough memory available.
-		mov si,(s1start + pt_off)
-		mov di,(pblock_addr + pt_off)
-		mov cx,(pt_size + sig_size)
-.pcopy:		lodsb
-		stosb
-		loop .pcopy
-
-		; Copy the remaining code.
-		;
-		; Maintain the same relative offsets for the same reasons as
-		; when copying the partition table.
 		mov si,s1end
 		mov di,(s1size + run_addr)
 		mov cx,s2size
-.ccopy:		lodsb
-		stosb
-		loop .ccopy
+		rep movsb
 
 		; Execute the copied code.
 		jmp s2start
+		
+		; Parse ASCII hex constant at DS:SI into binary ES:DI.
+		; Stop when a character other than '0'-'9' 'a'-'f' or '-' is
+		; encountered. '-' are NO-OPs.
+parse_hex:	xor ch,ch
+.next_digit:	lodsb
+		sub al,'0'
+		jae .lower_ok
+.no_more:	ret
+.lower_ok:	cmp al,10
+		jb .ok
+		sub al,('A' - ('9' + 1))
+		cmp al,16
+		jae .no_more
+.ok:		mov cl,4
+		shl ah,cl
+		add ah,al
+		inc ch
+		shr ch,1
+		jc .next_digit
+		mov al,ah
+		stosb
+		jmp .next_digit
+
 s1end:		; Start of source copy area.
 s1size		equ s1end - s1start
 
@@ -124,37 +146,59 @@ s1size		equ s1end - s1start
 		        vstart=(s1size + run_addr) align=1
 s2start:	mov dl,80h ; Drive number to operate on (0x80 = 1st hard disk).
 enum_drives:	mov ah,41h; Get int13 extensions supported by drive.
-		mov bx,[pblock_addr + sig_off]
+		mov bx,[run_addr + sig_off]
 		; Disk BIOS services may potentially trash: AX, SI, DI, BP, ES.
 		push es
 		int 13h
 		pop es
-		jc .nextdrive ; Command failed.
+		jc nextdrive ; Command failed.
 		mov ax,cx
 		shr ax, 1 ; Set CF to bit # 0 of feature bitmask.
-		jnc .nextdrive ; LBA packet addressing not supported.
-		mov si,msg
-		call puts
-		mov al,ah
-		xor ah,ah
-		push ax
-		mov al,dl
-		call putns ; Drive #.
-		mov ax,cx
-		call putns ; Feature mask.
-		pop ax
-		call putns ; Major version.
-.nextdrive:	inc dl
+		jnc nextdrive ; LBA packet addressing not supported.
+		
+		; Examine drive - set MBR at sector 0 to be examined next.
+		mov di,sector
+		xor ax,ax
+		stosw
+		stosw
+		stosw
+		stosw
+		
+enum_parttabs:	; Examine next partition table on current drive.
+		call read_sector
+		jc nextdrive ; Read error - ignore this drive.
+		
+		; Make a copy of the partition table for continued use.
+		mov si,(load_addr + pt_off)
+		mov di,(run_addr + pt_off)
+		mov cx,pt_size
+		rep movsb
+		
+		; Examine partition table of MBR or EBR in 2 phases.
+		mov si,(run_addr + pt_off)
+		
+nextdrive:	inc dl
 		mov dh,7fh
 		and dh,dl
 		cmp dh,[bda_hdd_count]
-		jae done
-		jmp enum_drives
+		jb enum_drives
 
-done:		mov si,endtext
+		mov si,not_found
 		call puts
 stop:		hlt
 		jmp stop
+
+read_sector:	; Read sector with currently set sector number in DAP.
+		push ax
+		push si
+		mov ah,42h ; Extended Read Sectors From Drive.
+		mov si,dap
+		push es
+		int 13h
+		pop es
+		pop si
+		pop ax
+		ret
 
 putn:		; Display an unsigned decimal integer in AX.
 		push ax
@@ -225,31 +269,44 @@ video:		; Displays character or string depending on code in AH.
 		pop si
 		ret
 
+dap_tpl:	; DAP - Disk Address Packet (template to be copied).
+.self_size:	db .struct_size - $
+.reserved:	db 0
+.numsects:	dw 1
+.bufoff:	dw load_addr
+.bufseg:	dw 0
+.sector		dq 0 ; Sector to read (0-based).
+.struct_size:
+
+dap		equ dap_tpl - load_addr + run_addr
+sector		equ dap_tpl.sector - load_addr + run_addr
+
+; Message text area.
+
 %define NL 13, 10 ; CR/LF is newline.
 %define LNUM 0, '.', NL, 0
 
-msg:		db 'Drive # ', 0
-		db ' supports features ', 0
-		db ' with major version ', LNUM
+debug:		db "Yeah - read a sector!", NL, 0
 
-bxmsg:		db 'BX=', 0, ' but should be ', LNUM
-
-numdrives:	db 'There are ', 0, ' HDDs present.', NL, 0
-
-endtext:	db 'End of list of disk devices.', NL, 0
-
-error:		db 'Disk error # ', LNUM
+not_found:	db 'NOT '
+found:		db 'Found ext2+ FS {'
+ascii_uuid:	db '67758eb7-5baa-4c14-ba21-cbf38d0180f3'
+		db '} ptype 0x'
+ascii_ptype:	db '83', NL, 0
 
 s2end:		; Start of unused space after used space.
 s2size		equ s2end - s2start
 
-code_space	equ s1size + s2size
-unused_code_space \
-		equ   sector_size - sig_size \
-		    - pt_size - sn_ind_size - sn_size - code_space
+; Gap between the used and unused part of the MBR.
+
+leading_data	equ s1size + s2size
+unused_space 	equ   sector_size - sig_size \
+		    - pt_size - sn_ind_size - sn_size - leading_data
 
 		; Fill the unused rest of the code space with NO-OPs.
-		times unused_code_space nop
+		times unused_space nop
+
+; Fixed data structures at the end of the MBR.
 
 		times sn_size db 'S' ; Invalid disk serial number.
 		times sn_ind_size db 0ffh ; Mark serial number as invalid.
